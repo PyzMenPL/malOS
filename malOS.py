a61a97a77f8ad2041d4d7027d0d23bf518ddf57f
Niskative <niskative@proton.me>
2022-12-22 21:04:57 +0100

    Forgor to add another line (I have to buy better glasses :D)

--- malOS.py
+++ malOS.py
@@ -322,5 +322,6 @@ class Folder(File):
 if __name__ == "__main__":
     print("Type 'help' to see all available commands")
     fastFS = Filesystem()
+    current_folder = "/"
     while True:
         current_folder = fastFS.command(input(current_folder + ' $ '))
